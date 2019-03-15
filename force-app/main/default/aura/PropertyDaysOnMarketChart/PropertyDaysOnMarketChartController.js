8bf8ce6391231cc1f539558fa433cab19400c606
Dave Carroll <116254+dcarroll@users.noreply.github.com>
2019-03-14 18:49:45 -0700

    first prune

--- force-app/main/default/aura/PropertyDaysOnMarketChart/PropertyDaysOnMarketChartController.js
+++
@@ -1,8 +0,0 @@
-({
-	recordUpdated : function(component, event, helper) {
-		var changeType = event.getParams().changeType;
-        if (changeType === "LOADED" || changeType === "CHANGED") {
-			helper.showDaysOnMarket(component);
-        }
-	}
-})
--- force-app/main/default/lwc/helloTest/helloTest.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class HelloTest extends LightningElement {
-    foo() {
-        console.log("IT WORKED THIS TIME")
-    }
-}
